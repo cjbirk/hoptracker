c8916cd2cb528585e7e6a915db62e92a2d32ca89
Charles Birk <charles@cryptolab.net>
2016-04-10 21:44:35 -0400

    First commit
    First commit

---
+++ api.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'db.php';
+// Connect to MySQL database
+$con = mysql_connect($host,$user,$password);
+$dbs = mysql_select_db($databaseName,$con);
+
+// Query database for data
+$items = array("hrcas", "hrcen", "hrchi", "hrcol", "hrcry", "hrgol", "hrmh", "hrnbr", "hrnug", "hrste", "hrtet", "hrwil");
+$index = 0;
+$arraytotal = array();
+class itemRecord { 
+	public $item_no;  
+	public $description;
+	public $price;
+	public $quantity_on_hand;
+	public $quantity_on_order;
+	public $specialorder;
+}
+foreach ($items as $item) {
+  $result 		     = mysql_query("SELECT item.item_no,item.description,item.price,item.quantity_on_hand,item.quantity_on_order,sum(specialorder.quantity) FROM item,specialorder WHERE item.item_no='$item' AND specialorder.item_no='$item';");
+  $array 		     = mysql_fetch_row($result);
+  $myItem 		     = new itemRecord;
+  $myItem->item_no 	     = $array[0];
+  $myItem->description       = $array[1];
+  $myItem->price	     = $array[2];
+  $myItem->quantity_on_hand  = $array[3];
+  $myItem->quantity_on_order = $array[4];
+  $myItem->specialorder      = $array[5];
+  $arraytotal[$index]  = $myItem;
+  $index++;
+  }
+
+echo json_encode($arraytotal);
+
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+
+  <head>
+   <title>HOP TRACKER</title>
+    <script language="javascript" type="text/javascript" src="js/jquery.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat|Roboto' rel='stylesheet' type='text/css'>
+
+    <script language="javascript" type="text/javascript" src="js/hoptrack.js"></script>
+
+  </head>
+
+  <body>
+
+     <style type="text/css">
+
+     </style>
+
+     <center><img src="img/hops.svg" height="100" width="100"></center>
+
+     <h2><center>HOP RHIZOME TRACKER</center></h2>
+
+     <center>This data on this page refreshes automatically every 3 seconds.</center></br>
+
+     <center><div id="output"></div></center>
+
+     <script id="source" language="javascript" type="text/javascript">
+
+     </script>
+
+     </br><center>Questions? Comments? Concerns? E-mail <a href="mailto:it@keystonehomebrew.com">Charles Birk</center>
+
+  </body>
+
+</html>
